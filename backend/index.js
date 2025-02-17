d8f9068110b607379405a90644ba2eac9a0b877c
feriel.benkraiem@ensi-uma.tn <$(AZURE_DEVOPS_EMAIL)>
2025-02-17 18:18:18 +0100

    test

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.get('/api', (req, res) => {
     res.json({ message: 'Fantasy API is running!' });
 });
-
+//test
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
